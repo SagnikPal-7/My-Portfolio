8200314374080e28df1b04ddf4da47873787edac
Sagnik Pal <131534396+SagnikPal-7@users.noreply.github.com>
2025-08-22 09:35:28 +0530

    hpe final change

--- src/components/cover/ProfileName.tsx
+++ src/components/cover/ProfileName.tsx
@@ -13,12 +13,10 @@ const ProfileName: FC<ProfileNameProps> = ({}) => {
         <li>
           🚀 Full-stack MERN developer passionate about building scalable web
           applications, with strong problem-solving skills demonstrated by
-          solving 350+ DSA questions on LeetCode.
+          solving 350+ DSA questions on LeetCode, with solid expertise in Java,
+          C++, and Python.
         </li>
-        <li>
-          💡 Currently exploring Machine Learning and Cloud Computing, with
-          solid expertise in Java, C++, and Python.
-        </li>
+        <li>💡 Currently exploring Machine Learning and Cloud Computing.</li>
         <Link
           className="block md:hidden"
           href="https://www.instagram.com/smp_philophobia_7/"
