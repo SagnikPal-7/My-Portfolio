38b2747a76594d7a9ac26ccc8b462f740049830c
Sagnik Pal <131534396+SagnikPal-7@users.noreply.github.com>
2025-08-22 09:33:21 +0530

    chne

--- src/components/cover/ProfileName.tsx
+++ src/components/cover/ProfileName.tsx
@@ -10,10 +10,15 @@ const ProfileName: FC<ProfileNameProps> = ({}) => {
     <div className="text-3xl flex flex-col font-bold text-highlight">
       Sagnik Pal
       <div className="mt-2 text-sm font-medium flex justify-between items-center">
-        🚀 Full-stack MERN developer passionate about building scalable web
-        applications, with strong problem-solving skills demonstrated by solving
-        350+ DSA questions on LeetCode. 💡 Currently exploring Machine Learning
-        and Cloud Computing, with solid expertise in Java, C++, and Python.
+        <li>
+          🚀 Full-stack MERN developer passionate about building scalable web
+          applications, with strong problem-solving skills demonstrated by
+          solving 350+ DSA questions on LeetCode.{" "}
+        </li>
+        <li>
+          💡 Currently exploring Machine Learning and Cloud Computing, with
+          solid expertise in Java, C++, and Python.
+        </li>
         <Link
           className="block md:hidden"
           href="https://www.instagram.com/smp_philophobia_7/"
